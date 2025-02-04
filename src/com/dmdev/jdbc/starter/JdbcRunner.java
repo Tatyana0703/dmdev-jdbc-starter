7d612c1572078f5d21869ec318dc8221c4e310c6
tmikhaylovskaya <tmikhaylovskaya1981@gmail.com>
2025-02-05 01:25:41 +0300

    мой update

--- src/com/dmdev/jdbc/starter/JdbcRunner.java
+++ src/com/dmdev/jdbc/starter/JdbcRunner.java
@@ -10,7 +10,7 @@ public class JdbcRunner {
     public static void main(String[] args) throws SQLException {
         Class<Driver> driverClass = Driver.class;
         try (var connection = ConnectionManager.open()) {
-            System.out.println(connection.getTransactionIsolation());
+            System.out.println("!!!!! TransactionIsolation " + connection.getTransactionIsolation());
         }
     }
 }
